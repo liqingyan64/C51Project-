8afdf2b023a11b0c09b4087329717ba3557ad8c3
liliqingyan64 <liliqingyan@gmail.com>
2026-06-22 18:50:52 +0800

    tune: set unlock animation frame delay to 30ms

--- source/password_lock.c
+++ source/password_lock.c
@@ -258,7 +258,7 @@ void unlock_animation_loop(void)
     while (1) {
         for (i = 0; i < UNLOCK_ANIM_FRAME_COUNT; i++) {
             show_anim_frame(i);
-            if (anim_wait_or_exit(70)) {
+            if (anim_wait_or_exit(30)) {
                 lcd_clear_graph();
                 return;
             }
